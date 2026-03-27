b79959e2800753c6f6a3a582a056e9062652a648
kenese <kenese.lautusi@gmail.com>
2026-03-27 19:58:57 +1300

    solution

--- web/public/app.js
+++ web/public/app.js
@@ -18,72 +18,96 @@
 
 
 // Add this to the top of index.js
-const { Observable } = rxjs;
+// const {Observable} = rxjs;
 
+class Observable {
+    callback
+    observers = new Set();
 
-
-
-
-
-class Observable1 {
-  callback
-  observers = new Set();
-
-  constructor(callback) {
-    this.callback = callback;
-  }
-
-  subscribe (observer) {
-    this.observers.add(observer);
-    this.callback({
-      next: (...props) => {
-        this.observers.values().forEach(observer => {
-          observer.next(...props);
-        })
-    }})
-
-    return () => {
-      this.observers.delete(observer);
+    constructor(callback) {
+        this.callback = callback;
     }
-  }
+
+    subscribe(observer) {
+        this.observers.add(observer);
+        const unsubscribe = this.callback({
+            next: (...props) => {
+                this.observers.values().forEach(observer => {
+                    try {
+                        observer.next(...props);
+                    } catch (e) {
+                        observer.error(e)
+                    }
+                })
+            },
+            error: (...props) => {
+                this.observers.values().forEach(observer => {
+                    observer.error(...props)
+                })
+            }
+        });
+
+        return {
+            unsubscribe: () => {
+                this.observers.delete(observer);
+                unsubscribe();
+            }
+        }
+    }
 }
 
-// Simple counter example
+// 1. Simple counter example
 const counter = new Observable(observer => {
-  console.log(observer);
-  for (let i = 0; i < 10; i++) {
-    observer.next(i);
-  }
+    console.log(observer);
+    for (let i = 0; i < 10; i++) {
+        observer.next(i);
+    }
 });
-counter.subscribe({ next: console.log }); // 0, 1, 2, 3 ... 9
-counter.subscribe({ next: console.log }); // 0, 1, 2, 3 ... 9
+counter.subscribe({next: console.log});
+counter.subscribe({next: console.log});
 
-//
-//
-// // Unsubscribe example
-// const toggleButton = document.getElementById('toggle');
-// const clickButton = document.getElementById('click');
-// const clickEvents = new Observable((observer) => {
-//   const handler = (event) => {
-//     return observer.next(event);
-//   }
-//   clickButton.addEventListener('click', handler, true);
-//   return () => {
-//     clickButton.removeEventListener('click', handler, true);
-//   };
-// });
-// let clickEventsSubscription;
-// function toggle() {
-//   if (clickEventsSubscription) {
-//     clickEventsSubscription.unsubscribe();
-//     clickEventsSubscription = undefined;
-//     toggleButton.innerText = 'Listen';
-//     return;
-//   }
-//   clickEventsSubscription = clickEvents.subscribe({ next: console.log });
-//   toggleButton.innerText = 'Stop Listening';
-// }
-// toggleButton.addEventListener('click', toggle);
-//
-//
-//
+
+// 2. Error Handling
+const errorObservable$ = new Observable(subscriber => {
+    try {
+        subscriber.next(1);
+        throw new Error('Something went wrong!');
+    } catch (err) {
+        subscriber.error(err); // Deliver the error notification
+    }
+});
+
+const observer = {
+    next: (value) => console.log('Received value:', value),
+    error: (err) => console.error('Observer caught an error:', err.message),
+};
+
+errorObservable$.subscribe(observer);
+
+
+// 3. Unsubscribe example
+const toggleButton = document.getElementById('toggle');
+const clickButton = document.getElementById('click');
+const clickEvents = new Observable((observer) => {
+    const handler = (event) => {
+        return observer.next(event);
+    }
+    clickButton.addEventListener('click', handler, true);
+    return () => {
+        clickButton.removeEventListener('click', handler, true);
+    };
+});
+let clickEventsSubscription;
+
+function toggle() {
+    if (clickEventsSubscription) {
+        clickEventsSubscription.unsubscribe();
+        clickEventsSubscription = undefined;
+        toggleButton.innerText = 'Listen';
+        return;
+    }
+    clickEventsSubscription = clickEvents.subscribe({next: console.log});
+    toggleButton.innerText = 'Stop Listening';
+}
+
+toggleButton.addEventListener('click', toggle);
