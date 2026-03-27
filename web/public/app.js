bbec0be50de208c49646fc23cbe9f5e7a3237c35
kenese <kenese.lautusi@gmail.com>
2026-03-27 19:37:10 +1300

    where i got to

--- web/public/app.js
+++ web/public/app.js
@@ -1,6 +1,89 @@
-import { tagline } from './message.js';
+// /** An Observable instance; can emit values at any time. Can be subscribed to by any number of observers */
+// interface Observable<T> {
+//   constructor(subscriber: (observer: Observer<T>) => UnsubscribeFn | undefined): Observable<T>;
+//   subscribe(observer: Observer<T>): Subscription<T>;
+// }
+// /** An Observer; Consumes values emitted by an observable */
+// interface Observer<T> {
+//   next(value: T): void;
+//   error(err: Error): void;
+//   complete(): void;
+// }
+// /** A Subscription instance describes the relationship between an observable and one of it's observers */
+// interface Subscription<T> {
+//   unsubscribe(): void;
+// }
+// /** A cleanup function that is called when unsubscribing to an Observable */
+// type UnsubscribeFn = () => void;
 
-const el = document.querySelector('#client-note');
-if (el) {
-  el.textContent = tagline();
+
+// Add this to the top of index.js
+const { Observable } = rxjs;
+
+
+
+
+
+
+class Observable1 {
+  callback
+  observers = new Set();
+
+  constructor(callback) {
+    this.callback = callback;
+  }
+
+  subscribe (observer) {
+    this.observers.add(observer);
+    this.callback({
+      next: (...props) => {
+        this.observers.values().forEach(observer => {
+          observer.next(...props);
+        })
+    }})
+
+    return () => {
+      this.observers.delete(observer);
+    }
+  }
 }
+
+// Simple counter example
+const counter = new Observable(observer => {
+  console.log(observer);
+  for (let i = 0; i < 10; i++) {
+    observer.next(i);
+  }
+});
+counter.subscribe({ next: console.log }); // 0, 1, 2, 3 ... 9
+counter.subscribe({ next: console.log }); // 0, 1, 2, 3 ... 9
+
+//
+//
+// // Unsubscribe example
+// const toggleButton = document.getElementById('toggle');
+// const clickButton = document.getElementById('click');
+// const clickEvents = new Observable((observer) => {
+//   const handler = (event) => {
+//     return observer.next(event);
+//   }
+//   clickButton.addEventListener('click', handler, true);
+//   return () => {
+//     clickButton.removeEventListener('click', handler, true);
+//   };
+// });
+// let clickEventsSubscription;
+// function toggle() {
+//   if (clickEventsSubscription) {
+//     clickEventsSubscription.unsubscribe();
+//     clickEventsSubscription = undefined;
+//     toggleButton.innerText = 'Listen';
+//     return;
+//   }
+//   clickEventsSubscription = clickEvents.subscribe({ next: console.log });
+//   toggleButton.innerText = 'Stop Listening';
+// }
+// toggleButton.addEventListener('click', toggle);
+//
+//
+//
