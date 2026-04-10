88a568e83db65625cc983c1a1b4871e4d9deddd6
kenese <kenese.lautusi@gmail.com>
2026-04-10 15:58:18 +1200

    very basic

--- web/public/app.js
+++ web/public/app.js
@@ -1,6 +1,23 @@
-import { tagline } from './message.js';
 
-const el = document.querySelector('#client-note');
+const el = document.querySelector('.board');
 if (el) {
-  el.textContent = tagline();
+  const image = new Image();
+  image.src = "/target.png";
+  image.classList.add('image');
+
+  image.style.left = '100px';
+  image.style.top = '100px';
+
+  image.onload = () => {
+    console.log("Image loaded successfully!");
+  };
+  image.onerror = () => {
+    console.error("Failed to load the image.");
+  };
+
+  el.appendChild(image);
+
+  debugger;
 }
+
+
