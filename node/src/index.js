c997777717cae3566587d449cdafa0383f2efba7
kenese <kenese.lautusi@gmail.com>
2026-04-11 15:39:17 +1200

    added debounced search

--- node/src/index.js
+++ node/src/index.js
@@ -1,206 +1,249 @@
-export class Observable {
-    subscriptions = new Set();
+// export class Observable {
+//     subscriptions = new Set();
+//
+//     constructor(subscriber) {
+//         this.subscription = subscriber;
+//     }
+//
+//     subscribe(observer) {
+//         this.subscriptions.add(observer);
+//
+//         this.subscription({
+//             next: (thing) => {
+//                 if (this.subscriptions.has(observer)) {
+//                     observer.next(thing);
+//                 }
+//             },
+//             complete: () => {
+//                 observer.complete();
+//             },
+//             error: err => {
+//                 if (this.subscriptions.has(observer)) {
+//                     observer.error(err);
+//                 }
+//             }
+//         })
+//
+//         return {
+//             unsubscribe: () => {
+//                 this.subscriptions.delete(observer);
+//             }
+//         }
+//     }
+// }
+//
+//
+// // Simple counter example
+// const counter = new Observable(observer => {
+//     for (let i = 0; i < 10; i++) {
+//         observer.next(i);
+//     }
+//     observer.complete();
+// });
+// counter.subscribe({next: console.log, complete: () => console.log('completed 1')}); // 0, 1, 2, 3 ... 9
+// counter.subscribe({next: console.log, complete: () => console.log('completed 2')}); // 0, 1, 2, 3 ... 9
+//
+//
+// // 2. Error Handling
+// const errorObservable$ = new Observable(subscriber => {
+//     try {
+//         subscriber.next(1);
+//         throw new Error('Something went wrong!');
+//     } catch (err) {
+//         subscriber.error(err); // Deliver the error notification
+//     }
+// });
+//
+// const observer = {
+//     next: (value) => console.log('Received value:', value),
+//     error: (err) => console.error('Observer caught an error:', err.message),
+//     complete: () => console.log('Completed:')
+// };
+//
+// errorObservable$.subscribe(observer);
+//
+//
+// // 3. Unsubscribe example
+// const toggleButton = document.getElementById('toggle');
+// const clickButton = document.getElementById('click');
+// const clickEvents = new Observable((observer) => {
+//     const handler = (event) => {
+//         return observer.next(event);
+//     }
+//     clickButton.addEventListener('click', handler, true);
+//
+//     return () => {
+//         clickButton.removeEventListener('click', handler, true);
+//     };
+// });
+// let clickEventsSubscription;
+//
+// function toggle() {
+//     if (clickEventsSubscription) {
+//         clickEventsSubscription.unsubscribe();
+//         clickEventsSubscription = undefined;
+//         toggleButton.innerText = 'Listen';
+//         return;
+//     }
+//     clickEventsSubscription = clickEvents.subscribe({next: console.log});
+//     toggleButton.innerText = 'Stop Listening';
+// }
+//
+// toggleButton.addEventListener('click', toggle);
+//
+//
+// // Promise
+//
+// // executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void): Promise<T>;
+//
+// class PromiseNew {
+//     constructor(executor) {
+//         this.executor = executor;
+//     }
+//
+//     then(onFullFilled, onRejected) {
+//         const thing = this.executor(onFullFilled, onRejected);
+//         debugger;
+//         return this;
+//     }
+// }
+//
+// const PENDING = 'pending';
+// const FULFILLED = 'fulfilled';
+// const REJECTED = 'rejected';
+//
+// class MyPromise {
+//     constructor(executor) {
+//         this.state = PENDING;
+//         this.value = null;
+//         this.handlers = [];
+//
+//         // The executor runs immediately and receives resolve/reject functions
+//         try {
+//             executor(this._resolve, this._reject);
+//         } catch (err) {
+//             this._reject(err);
+//         }
+//     }
+//
+//     _resolve = (value) => {
+//         if (this.state !== PENDING) return;
+//
+//         this.state = FULFILLED;
+//         this.value = value;
+//         this._executeHandlers();
+//     }
+//
+//     _reject = (error) => {
+//         if (this.state !== PENDING) return;
+//
+//         this.state = REJECTED;
+//         this.value = error;
+//         this._executeHandlers();
+//     }
+//
+//     then(onFulfilled, onRejected) {
+//         this.handlers.push({onFulfilled, onRejected});
+//
+//         // If the promise is already settled, run handlers immediately
+//         if (this.state !== PENDING) {
+//             this._executeHandlers();
+//         }
+//
+//         return this;
+//     }
+//
+//     _executeHandlers() {
+//         if (this.state === PENDING) return;
+//
+//         this.handlers.forEach((handler) => {
+//             debugger;
+//             if (this.state === FULFILLED) {
+//                 if (typeof handler.onFulfilled === 'function') {
+//                     this.value = handler.onFulfilled(this.value);
+//                 }
+//             } else {
+//                 if (typeof handler.onRejected === 'function') {
+//                     this.value = handler.onRejected(this.value);
+//                 }
+//             }
+//         });
+//
+//         debugger;
+//         this.handlers = []; // Clear handlers after execution
+//     }
+//
+// }
+//
+//
+// const myPromise = new MyPromise((resolve, reject) => {
+//     console.log('Promise setup');
+//     // reject('nah');
+//     try {
+//         // throw "nah";
+//         setTimeout(() => {
+//             console.log('Promise resolving');
+//             resolve("foo");
+//         }, 300);
+//     } catch (err) {
+//         reject(err);
+//     }
+// });
+//
+// const thing = myPromise.then((value) => {
+//     console.log(value);
+//     throw value;
+//     // return value + ' baa';
+// }, (err) => {
+//     console.error(err);
+//     return 'twas an error';
+// }).then(value => {
+//     console.log(value);
+// }, console.error)
 
-    constructor(subscriber) {
-        this.subscription = subscriber;
+
+class TaskRunner {
+    constructor(maxConcurrent) {
+        this.maxConcurrent = maxConcurrent;
+        this.queue = [];
+        this.running = 0;
     }
 
-    subscribe(observer) {
-        this.subscriptions.add(observer);
+    run() {
+        const task = this.queue.shift();
+        if (!task) return;
 
-        this.subscription({
-            next: (thing) => {
-                if (this.subscriptions.has(observer)) {
-                    observer.next(thing);
-                }
-            },
-            complete: () => {
-                observer.complete();
-            },
-            error: err => {
-                if (this.subscriptions.has(observer)) {
-                    observer.error(err);
-                }
-            }
-        })
-
-        return {
-            unsubscribe: () => {
-                this.subscriptions.delete(observer);
-            }
-        }
-    }
-}
-
-
-// Simple counter example
-const counter = new Observable(observer => {
-    for (let i = 0; i < 10; i++) {
-        observer.next(i);
-    }
-    observer.complete();
-});
-counter.subscribe({next: console.log, complete: () => console.log('completed 1')}); // 0, 1, 2, 3 ... 9
-counter.subscribe({next: console.log, complete: () => console.log('completed 2')}); // 0, 1, 2, 3 ... 9
-
-
-// 2. Error Handling
-const errorObservable$ = new Observable(subscriber => {
-    try {
-        subscriber.next(1);
-        throw new Error('Something went wrong!');
-    } catch (err) {
-        subscriber.error(err); // Deliver the error notification
-    }
-});
-
-const observer = {
-    next: (value) => console.log('Received value:', value),
-    error: (err) => console.error('Observer caught an error:', err.message),
-    complete: () => console.log('Completed:')
-};
-
-errorObservable$.subscribe(observer);
-
-
-// 3. Unsubscribe example
-const toggleButton = document.getElementById('toggle');
-const clickButton = document.getElementById('click');
-const clickEvents = new Observable((observer) => {
-    const handler = (event) => {
-        return observer.next(event);
-    }
-    clickButton.addEventListener('click', handler, true);
-
-    return () => {
-        clickButton.removeEventListener('click', handler, true);
-    };
-});
-let clickEventsSubscription;
-
-function toggle() {
-    if (clickEventsSubscription) {
-        clickEventsSubscription.unsubscribe();
-        clickEventsSubscription = undefined;
-        toggleButton.innerText = 'Listen';
-        return;
-    }
-    clickEventsSubscription = clickEvents.subscribe({next: console.log});
-    toggleButton.innerText = 'Stop Listening';
-}
-
-toggleButton.addEventListener('click', toggle);
-
-
-
-
-
-
-
-// Promise
-
-// executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void): Promise<T>;
-
-class PromiseNew {
-    constructor(executor) {
-        this.executor = executor;
-    }
-    then (onFullFilled, onRejected) {
-        const thing = this.executor(onFullFilled, onRejected);
-        debugger;
-        return this;
-    }
-}
-
-const PENDING = 'pending';
-const FULFILLED = 'fulfilled';
-const REJECTED = 'rejected';
-
-class MyPromise {
-    constructor(executor) {
-        this.state = PENDING;
-        this.value = null;
-        this.handlers = [];
-
-        // The executor runs immediately and receives resolve/reject functions
-        try {
-            executor(this._resolve, this._reject);
-        } catch (err) {
-            this._reject(err);
-        }
-    }
-
-    _resolve = (value) => {
-        if (this.state !== PENDING) return;
-
-        this.state = FULFILLED;
-        this.value = value;
-        this._executeHandlers();
-    }
-
-    _reject = (error) => {
-        if (this.state !== PENDING) return;
-
-        this.state = REJECTED;
-        this.value = error;
-        this._executeHandlers();
-    }
-
-    then(onFulfilled, onRejected) {
-        this.handlers.push({ onFulfilled, onRejected });
-
-        // If the promise is already settled, run handlers immediately
-        if (this.state !== PENDING) {
-            this._executeHandlers();
-        }
-
-        return this;
-    }
-
-    _executeHandlers() {
-        if (this.state === PENDING) return;
-
-        this.handlers.forEach((handler) => {
-            debugger;
-            if (this.state === FULFILLED) {
-                if (typeof handler.onFulfilled === 'function') {
-                    this.value = handler.onFulfilled(this.value);
-                }
-            } else {
-                if (typeof handler.onRejected === 'function') {
-                    this.value = handler.onRejected(this.value);
-                }
-            }
+        this.running++;
+        task().then(() => {
+            this.running--;
+            this.run();
         });
-
-        debugger;
-        this.handlers = []; // Clear handlers after execution
     }
 
+    push(taskToQueue) {
+        this.queue.push(taskToQueue);
+        if (this.running < this.maxConcurrent) {
+            this.run()
+        }
+    }
 }
 
 
-const myPromise = new MyPromise((resolve, reject) => {
-    console.log('Promise setup');
-    // reject('nah');
-    try {
-        // throw "nah";
-        setTimeout(() => {
-            console.log('Promise resolving');
-            resolve("foo");
-        }, 300);
-    } catch (err) {
-        reject(err);
-    }
-});
+const runner = new TaskRunner(3); // Limit to 2 concurrent tasks
 
-const thing = myPromise.then((value) => {
-    console.log(value);
-    throw value;
-    // return value + ' baa';
-}, (err) => {
-    console.error(err);
-    return 'twas an error';
-}).then(value => {
-    console.log(value);
-}, console.error)
+const task = (id, ms) => () =>
+    new Promise(resolve => setTimeout(() => {
+        console.log(`Task ${id} done`);
+        resolve(id);
+    }, ms));
+
+// Should start immediately
+runner.push(task(1, 5000));
+runner.push(task(2, 2000));
+runner.push(task(3, 1000));
+runner.push(task(4, 500));
+runner.push(task(5, 1000));
+runner.push(task(6, 100));
+
+// Should wait until Task 2 finishes (at 500ms) before starting
+runner.push(task(7, 100));
--- web/public/app.js
+++ web/public/app.js
@@ -1,6 +1,72 @@
-import { tagline } from './message.js';
 
-const el = document.querySelector('#client-note');
-if (el) {
-  el.textContent = tagline();
+function fetchSuggestions(query, signal) {
+    return new Promise((resolve) => {
+        const suggestions = ["Poster", "Instagram Post", "Presentation", "Video", "Resume", "Logo", "Flyer"]
+            .filter(item => item.toLowerCase().includes(query.toLowerCase()));
+
+        const timeoutId = setTimeout(() => {
+            resolve(suggestions);
+        }, Math.random() * 900 + 100);
+
+        signal.onabort = () => {
+            console.log('aborting', query);
+            clearTimeout(timeoutId);
+        };
+    });
 }
+
+class SearchController {
+    timeoutId = null;
+    abortController = null;
+
+    constructor(props) {
+        this.searchInput = document.getElementById('search-input');
+        this.resultsList = document.getElementById('results-list');
+
+        this.setupListener();
+    }
+
+    setupListener() {
+        this.searchInput.addEventListener('input', async (e) => {
+            if (this.timeoutId) {
+                console.log('clear timeout');
+                clearTimeout(timeoutId);
+            }
+            this.timeoutId = setTimeout(async () => {
+
+                if (this.abortController) {
+                    this.abortController.abort()
+                }
+                this.timeoutId = null;
+                this.abortController = new AbortController();
+
+                const text = e.target.value;
+                console.log('fetching:', text);
+
+                this.results = await fetchSuggestions(text, this.abortController.signal);
+                console.log('render results:', this.results);
+
+                this.updateDOM();
+            }, 10);
+        })
+    }
+
+    updateDOM() {
+        const fragment = document.createDocumentFragment();
+
+        this.results.forEach(item => {
+            const el = document.createElement('div');
+            el.textContent = item;
+            fragment.appendChild(el);
+        });
+        this.resultsList.innerHTML = '';
+        this.resultsList.appendChild(fragment);
+    }
+}
+
+new SearchController();
+
+
+
+
+
