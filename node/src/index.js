704d3bba86f526481ad8f9ae1a7c4654b287a8a0
kenese <kenese.lautusi@gmail.com>
2026-04-11 17:12:40 +1200

    comment class done

--- node/src/index.js
+++ node/src/index.js
@@ -1,9 +1,81 @@
+
+export class Comments {
+
+    comments = new Map();
+
+    addComment(text, parentId) {
+        const id = crypto.randomUUID();
+
+        this.comments.set(id, {
+            id,
+            text,
+            parentId,
+            replies: []
+        });
+
+        if (parentId && this.comments.has(parentId)) {
+            const parentComment = this.comments.get(parentId);
+            this.comments.set(parentId, {
+                ...parentComment,
+                replies: [...parentComment.replies, id]
+            });
+        }
+        return id;
+    }
+
+    getComment(commentId) {
+        const comment = this.comments.get(commentId);
+        if (!comment) return;
+
+        return {
+            ...comment,
+            replies: comment.replies.map(reply => this.getComment(reply))
+        }
+    }
+
+    deleteComment(commentId) {
+        const comment = this.comments.get(commentId);
+        if (!comment) return;
+        this.comments.delete(commentId);
+
+        // remove self from parents reply array
+        if (comment.parentId && this.comments.has(comment.parentId)) {
+            const parentComment = this.comments.get(comment.parentId);
+            this.comments.set(comment.parentId, {
+                ...parentComment,
+                replies: parentComment.replies.filter(reply => reply !== commentId)
+            });
+        }
+
+        // remove all children
+        if (comment.replies.length) {
+            comment.replies.forEach((replyId) => {
+                this.deleteComment(replyId);
+            })
+        }
+    }
+}
+
 const comments = new Comments();
 
 const A = comments.addComment('AAA');
-const B = comments.addComment('BBB', A);
+const B = comments.addComment('BBB');
 const C = comments.addComment('CCC', A);
 const D = comments.addComment('DDD', B);
 const E = comments.addComment('EEE', C);
+const F = comments.addComment('FFF', C);
 
-console.log(comments.getComments());
+console.log(A);
+console.log(B);
+console.log(C);
+console.log(D);
+console.log(E);
+console.log(F);
+
+console.log(comments.getComment(A));
+console.log(comments.getComment(B));
+console.log(comments.getComment(C));
+console.log(comments.getComment(D));
+console.log(comments.getComment(E));
+console.log(comments.getComment(F));
+
--- node/test/thing.test.js
+++ node/test/thing.test.js
@@ -1,17 +1,61 @@
 import { describe, it } from 'node:test';
 import assert from 'node:assert';
-import { Thing } from '../src/Thing.js';
+import { Comments } from '../src/index.js';
 
 describe('Thing', () => {
-  it('defaults world to "world" and hello() uses it', () => {
-    const thing = new Thing();
-    assert.strictEqual(thing.world, 'world');
-    assert.strictEqual(thing.hello(), 'hello world');
+  it('getComment returns rich reply object', () => {
+    const comments = new Comments();
+
+    const A = comments.addComment('AAA');
+    const B = comments.addComment('BBB');
+    const C = comments.addComment('CCC', A);
+
+    assert.strictEqual(comments.getComment(A).replies[0].id, C);
   });
 
-  it('accepts a custom suffix for world and hello()', () => {
-    const thing = new Thing('there');
-    assert.strictEqual(thing.world, 'there');
-    assert.strictEqual(thing.hello(), 'hello there');
+  it('getComment returns rich reply object and all children', () => {
+    const comments = new Comments();
+
+    const A = comments.addComment('AAA');
+    const B = comments.addComment('BBB');
+    const C = comments.addComment('CCC', A);
+    const D = comments.addComment('DDD', B);
+    const E = comments.addComment('EEE', C);
+    const F = comments.addComment('FFF', C);
+
+    assert.strictEqual(comments.getComment(A).replies[0].id, C);
+    assert.strictEqual(comments.getComment(A).replies[0].replies[0].id, E);
+    assert.strictEqual(comments.getComment(A).replies[0].replies[1].id, F);
+  });
+
+  it('deleteComment removes all children', () => {
+    const comments = new Comments();
+
+    const A = comments.addComment('AAA');
+    const B = comments.addComment('BBB');
+    const C = comments.addComment('CCC', A);
+    const D = comments.addComment('DDD', B);
+    const E = comments.addComment('EEE', C);
+    const F = comments.addComment('FFF', C);
+
+    comments.deleteComment(A);
+
+    assert.strictEqual(comments.comments.size, 2);
+    assert.strictEqual(comments.getComment(A), undefined);
+    assert.strictEqual(comments.getComment(C), undefined);
+    assert.strictEqual(comments.getComment(E), undefined);
+    assert.strictEqual(comments.getComment(F), undefined);
+  });
+
+  it('deleteComment removes id from parents replies', () => {
+    const comments = new Comments();
+
+    const A = comments.addComment('AAA');
+    const B = comments.addComment('BBB');
+    const C = comments.addComment('CCC', A);
+
+    comments.deleteComment(C);
+
+    assert.deepEqual(comments.comments.get(A).replies, []);
   });
 });
