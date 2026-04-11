5aac9ce8b5e0645f16f763442a5edd43b025d511
kenese <kenese.lautusi@gmail.com>
2026-04-11 16:30:27 +1200

    very basic

--- node/src/index.js
+++ node/src/index.js
@@ -1,5 +1,9 @@
-import { Thing } from './Thing.js';
+const comments = new Comments();
 
-const thing = new Thing();
+const A = comments.addComment('AAA');
+const B = comments.addComment('BBB', A);
+const C = comments.addComment('CCC', A);
+const D = comments.addComment('DDD', B);
+const E = comments.addComment('EEE', C);
 
-console.log(thing.hello());
+console.log(comments.getComments());
